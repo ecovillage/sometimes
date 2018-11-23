a87a0dbe846a70498bde929ff8bf91a285e39809
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2018-11-23 15:51:18 +0100

    bold Definition implementation, depends on attr_extras

--- lib/sometimes.rb
+++ lib/sometimes.rb
@@ -1,5 +1,24 @@
 require "sometimes/version"
 
+require 'tty-config'
+require 'attr_extras'
+
 module Sometimes
-  # Your code goes here...
+  # Struct?
+  class BackupDefinition
+    rattr_initialize :path, :key, :comment, :store_size, :user, :host, :type, :what
+  end
+
+  def self.read_def name
+    config = TTY::Config.new
+    config.read name
+    BackupDefinition.new(config.fetch(:path),
+                         config.fetch(:key ),
+                         config.fetch(:comment),
+                         config.fetch(:store_size),
+                         config.fetch(:user),
+                         config.fetch(:host),
+                         config.fetch(:type),
+                         config.fetch(:what))
+  end
 end
