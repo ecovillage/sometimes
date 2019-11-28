782df6352edd027518cd5c9be26d8e1c88e27136
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-11-28 16:41:16 +0100

    add test for FileEnvironment.preparegit add -p test/file_environment_test.rb sometimes.gemspec

--- test/file_environment_test.rb
+++ test/file_environment_test.rb
@@ -1,6 +1,9 @@
 require "test_helper"
+require 'test_construct'
 
 class FileEnvironmentTest < Minitest::Test
+  include TestConstruct::Helpers
+
   def test_path_to_last_file
     definition = Sometimes::BackupDefinition.new path: '/backups/def/'
     assert_equal '/backups/def/last.tgz', Sometimes::FileEnvironment.last_file_link_path(definition)
@@ -11,6 +14,33 @@ class FileEnvironmentTest < Minitest::Test
     assert_equal 'tgz', Sometimes::FileEnvironment.file_extension(definition)
     definition = Sometimes::BackupDefinition.new type: 'mysql'
     assert_equal 'sql', Sometimes::FileEnvironment.file_extension(definition)
+  end
+
+  def test_prepare!
+    within_construct do |construct|
+      definition = Sometimes::BackupDefinition.new(
+        path: 'backups/def',
+        key:  'key',
+        store_size: {
+          daily:   1,
+          weekly:  1,
+          monthly: 1,
+          yearly:  1
+        },
+        type: 'tgz',
+        version: 1
+      )
+
+      refute File.exist? 'backups'
+      refute File.directory? 'backups/def'
+      refute File.directory? 'backups/daily'
+
+      Sometimes::FileEnvironment.prepare! definition
+
+      assert File.exist? 'backups'
+      assert File.directory? 'backups/def'
+      assert File.directory? 'backups/def/daily'
+    end
   end
 end
 
